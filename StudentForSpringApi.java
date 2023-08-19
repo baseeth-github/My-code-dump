d24417fbceb39b73d039df4403fa7b00c66021bd
Baseeth <baseethsk1503@gmail.com>
2023-08-19 18:05:43 +0530

    Added springApi practice classes SpringApp and StudentForSpringApi

---
+++ StudentForSpringApi.java
@@ -0,0 +1,35 @@
+package org.springss;
+
+public class StudentForSpringApi {
+    private String name;
+    private int marks;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", marks=" + marks +
+                '}';
+    }
+
+    public StudentForSpringApi(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+}
