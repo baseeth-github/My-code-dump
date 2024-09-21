7c5c6b2065b1e4a43336b1365ef00996481cd940
Baseeth <baseethsk1503@gmail.com>
2024-09-21 11:41:09 +0530

    Added index method in b1

---
+++ index.java
@@ -0,0 +1,7 @@
+public static void main(String[] args) {
+    //Hi there am going to introduce index method
+    public void index(){
+        int a=10; int b=14;
+        System.out.println(a+b);
+    }
+}
