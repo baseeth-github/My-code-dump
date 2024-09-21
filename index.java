3043e553e2d110040e3f673b1bed327863cf934f
Baseeth <baseethsk1503@gmail.com>
2024-09-21 11:44:26 +0530

    commiting index.java in b2

--- index.java
+++ index.java
@@ -2,6 +2,6 @@ public static void main(String[] args) {
     //Hi there am going to introduce index method
     public void index(){
         int a=10; int b=14;
-        System.out.println(a+b);
+        System.out.println(a-b);
     }
 }
