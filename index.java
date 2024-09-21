bd0bc65c14be050c7fc6d1c0ef84f639691d7a22
Baseeth <baseethsk1503@gmail.com>
2024-09-21 11:51:58 +0530

    3rd commit of index in b2

--- index.java
+++ index.java
@@ -1,8 +1,8 @@
 public static void main(String[] args) {
     //Hi there am going to introduce index method
     public void index(){
-        int a=10; int b=14;
-        System.out.println(a-b);
+        int a=13; int b=0;
+        //here are some operations
         System.out.println(a%b);
         System.out.println(a/b);
         System.out.println(a+b);
